ec30fb852384b8f080c39492caa8c18281ed02a4
dawright22 <dr@hashicorp.com>
2024-07-16 16:04:32 +1000

    first

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,11 +6,11 @@ variable "region" {
 //vault address
 variable "vault_addr" {
   type    = string
-  default = "https://vault-public-vault-568210d2.b06d58cb.z1.hashicorp.cloud:8200"
+  default = "https://vault-your-hcp-instance.hashicorp.cloud:8200"
 }
 
 //vault token
 variable "vault_token" {
   type    = string
-  default = "hvs.CAESIMsG-_xBwtGbIJ6SIcYSqSwBB6XPIfN3VXJJSeUvoy-dGigKImh2cy5xV0FjMUVybDR0MUlNTHAxSjlJa3pjUVAuVWhUQTcQ-ecl"
+  default = "Your-hcp-vault-token-here"
 }
